c1a111f9b27910a986116c5f53c1f4402d517183
TheMshary <MsharyPersonal@gmail.com>
2025-06-03 21:28:23 +0300

    Added Google Analytics Tag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import Script from "next/script";
 import StatsCalculator from './components/StatsCalculator';
 
 const Home = () => {
   return (
-    <div>
-      <StatsCalculator />
-    </div>
+    <>
+      <Script id="next"
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=G-GCSJBYL1H7`}>
+      </Script>
+      <Script id="next">
+        {
+          `window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-GCSJBYL1H7');`
+        }
+      </Script>
+
+      <div>
+        <StatsCalculator />
+      </div>
+    </>
   )
 }
 
